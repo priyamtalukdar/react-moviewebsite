94599a98df3d5c14108e973c708b8ddadf4f14fc
priyamtalukdar <tpriyam398@gmail.com>
2023-04-21 02:45:44 +0530

    modified

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+                <Link to="/"><img className="header__icon" src="https://banner2.cleanpng.com/20190730/shy/kisspng-photographic-film-movie-camera-cinema-website-and-mobile-application-development-service-5d3fc924ce3b33.8538265315644613488447.jpg" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Most Viewed Movies</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Highest Rated </span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming Movies</span></Link>
             </div>
         </div>
     )
--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -51,14 +51,14 @@ const Movie = () => {
                         </div>
                     </div>
                     <div className="movie__detailRightBottom">
-                        <div className="synopsisText">Synopsis</div>
+                        <div className="synopsisText">Movie Detail</div>
                         <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
                     </div>
                     
                 </div>
             </div>
             <div className="movie__links">
-                <div className="movie__heading">Useful Links</div>
+                <div className="movie__heading"> Links to official page</div>
                 {
                     currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__homeButton movie__Button">Homepage <i className="newTab fas fa-external-link-alt"></i></span></p></a>
                 }
